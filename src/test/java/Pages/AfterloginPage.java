719a7bffe17fa4408708735d17962819285be88b
DmytroSych <sich.dmitry@gmail.com>
2018-03-17 13:24:13 +0200

    AfterLogin xpath change

--- src/test/java/Pages/AfterloginPage.java
+++ src/test/java/Pages/AfterloginPage.java
@@ -4,10 +4,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class AfterloginPage extends BasePage{
-    @FindBy(xpath = "//li/span[@class='sn_menu_title']")
+    @FindBy(xpath = "//span[@class='sn_menu_title']")
     private WebElement userEmail;
 
    public String getUserEmail() {
         return userEmail.getText();
     }
 }
+
+
