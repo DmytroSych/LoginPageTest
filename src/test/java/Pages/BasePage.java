b8e8a51ca181afb0416f4493c2cd66372bae5b3b
DmytroSych <sich.dmitry@gmail.com>
2018-03-24 13:58:33 +0200

    AfterLogin xpath change

--- src/test/java/Pages/BasePage.java
+++ src/test/java/Pages/BasePage.java
@@ -5,8 +5,11 @@ import com.company.Driver.MyDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.logging.Logger;
+
 public class BasePage {
-    static public MyDriver driver = MyDriver.getMyDriver();
+    protected Logger log = Logger.getLogger(this.getClass().getSimpleName());
+    protected static MyDriver driver = MyDriver.getMyDriver();
 
     public BasePage() {
         PageFactory.initElements(driver, this);
--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,13 +1,17 @@
 package Pages;
 
+import org.eclipse.jetty.util.thread.strategy.ExecuteProduceConsume;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.awt.*;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
-public class LoginPage extends BasePage{
+public class LoginPage extends BasePage {
     @FindBy(name = "login")
     private WebElement inputlogin;
 
@@ -17,14 +21,20 @@ public class LoginPage extends BasePage{
     @FindBy(xpath = "//p/input[@type='submit']")
     private WebElement btnlogin;
 
+    @FindBy(name = "domn")
+    private WebElement domn;
+
+
     @FindBys(@FindBy(xpath = ""))
     private List<WebElement> mails;
 
 
     public void login(String name, String password) {
 
-        driver.jsSetValue(inputlogin,name);
-        driver.jsSetValue(inputpassword,password);
+        log.info("set input name - " + name);
+        driver.jsSetValue(inputlogin, name);
+        log.info("set input name - " + password);
+        driver.jsSetValue(inputpassword, password);
         btnlogin.click();
     }
 
